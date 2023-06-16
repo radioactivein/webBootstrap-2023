984713e34d98fc3cba0e0ba38b9a8eddd09ddca1
Al Ameen <78306701+radioactivein@users.noreply.github.com>
2023-06-16 14:08:23 +0530

    js

---
+++ Practice/js/owl-nav.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:2,
+            loop:false
+        }
+    }
+})
